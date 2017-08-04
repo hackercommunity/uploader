224e28815cfbdde7180fd7baf7e33a15070ab982
mwear3robloxandroid <mitchellwears@gmail.com>
2017-08-04 14:17:21 -0400

    Update Upload.php

--- Upload.php
+++ Upload.php
@@ -1,17 +1,17 @@
 <?php
-$target_dir = "uploads/";
+$target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$FileType = exe; pathinfo($target_file,PATHINFO_EXTENSION);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual exe or fake exe
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an exe - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not is not a exe";
-        $uploadOk = 0;
-    }
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an exe - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an exe.";
+        $uploadOk = 0;
+    }
 }
 ?>
